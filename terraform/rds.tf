f3f24b3969a516976c128eedf751eac8f5f1722d
Sagar S H <158994310+Sagarrrsh@users.noreply.github.com>
2026-01-20 18:21:15 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -24,7 +24,7 @@ resource "aws_db_instance" "postgres" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds.id]
 
-  backup_retention_period = 0
+  backup_retention_period = 7
   skip_final_snapshot     = true
   publicly_accessible     = false
 
@@ -32,3 +32,4 @@ resource "aws_db_instance" "postgres" {
     Name = "${var.project_name}-postgres"
   }
 }
+
